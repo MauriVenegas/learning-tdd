53f7c1a3315107030d548f5489feb460437531b1
Mauricio Venegas <mvenegas.developer@gmail.com>
2023-05-18 20:21:53 +1000

    TDD con reto de programación

---
+++ src/can-reconfigure.js
@@ -0,0 +1,22 @@
+// Explicación del ejeercicio: https://2021.adventjs.dev/challenges/23
+
+export const canReconfigure = (from, to) => {
+  if (typeof from !== "string") throw new Error("from is not a string");
+  if (typeof to !== "string") throw new Error("to is not a string");
+
+  if (from.length === to.length) return false;
+  if (new Set(from).size !== new Set(to).size) return false;
+
+  const transformations = {};
+
+  for (let i = 0; i < from.length; i++) {
+    const fromLetter = from[i];
+    const toLetter = to[i];
+
+    const storedLetter = transformations[fromLetter];
+    if (storedLetter && storedLetter !== toLetter) return false;
+
+    transformations[fromLetter] = toLetter;
+  }
+  return true;
+};
---
+++ test/can-reconfigure.test.js
@@ -0,0 +1,36 @@
+import { describe, expect, test } from "vitest";
+import { canReconfigure } from "../src/can-reconfigure";
+
+describe("canReconfigure", () => {
+  // test('should be a function', () => {
+  //   expect(canReconfigure).toBeTypeOf("function");
+  // })
+
+  test("should throw if first parameteris missing", () => {
+    expect(() => canReconfigure()).toThrow();
+  });
+
+  test("should throw if first parameteris is not a string", () => {
+    expect(() => canReconfigure(2)).toThrow();
+  });
+
+  test("should throw if second parameteris is not a string", () => {
+    expect(() => canReconfigure("a", 3)).toThrow();
+  });
+
+  test("should return a bolean", () => {
+    expect(canReconfigure("a", "b")).toBeTypeOf("boolean");
+  });
+
+  test("should return false if string provided have diferent length even with same unique letters", () => {
+    expect(canReconfigure("aab", "ab")).toBe(false);
+  });
+
+  test("should return false if string provided have diferent number of unique letters", () => {
+    expect(canReconfigure("abc", "ddd")).toBe(false);
+  });
+
+  test("should returns false if have different order of transformation", () => {
+    expect(canReconfigure("XBOX", "XXBO")).toBe(false);
+  });
+});
